113f08fbd1581b17eddb9af1f7b54cd35266e170
Khaled Albarawy <kalbarawy@raisaenergy.com>
2020-05-02 01:48:26 +0200

    [FEAT] Added PlaylistCard component

--- src/components/MediaCard.component.vue
+++ src/components/MediaCard.component.vue
@@ -9,7 +9,16 @@
       :publishedAt="item.date | getYearsAgo"
     />
     <ChannelCard
-      v-if="item.type === 'channel'"
+      v-else-if="item.type === 'channel'"
+      :id="item.id"
+      :title="item.title"
+      :channel="item.channelTitle"
+      :thumbnail="item.thumbnailURL"
+      :description="item.description"
+      :publishedAt="item.date | getYearsAgo"
+    />
+    <PlaylistCard
+      v-else-if="item.type === 'playlist'"
       :id="item.id"
       :title="item.title"
       :channel="item.channelTitle"
@@ -23,12 +32,14 @@
 <script>
 import VideoCard from './VideoCard.component.vue';
 import ChannelCard from './ChannelCard.component.vue';
+import PlaylistCard from './PlaylistCard.component.vue';
 import { getYearsAgo } from '../shared/services/helpers';
 
 export default {
   components: {
     VideoCard,
     ChannelCard,
+    PlaylistCard,
   },
 
   props: {
@@ -40,30 +51,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss" scoped>
-.media-card {
-  display: flex;
-  margin-bottom: 1em;
-  &__thumbnail {
-  }
-  &__details {
-    margin-left: 1em;
-    display: flex;
-    flex-direction: column;
-
-    &__title {
-      font-weight: normal;
-    }
-
-    &__meta {
-      color: #8b8b8b;
-      margin-bottom: 0.5em;
-    }
-
-    &__description {
-      color: #8b8b8b;
-    }
-  }
-}
-</style>
---
+++ src/components/PlaylistCard.component.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="playlist-card">
+    <div class="playlist-card__thumbnail">
+      <img :src="thumbnail" alt="video-thumbnail" />
+      <PlaylistIcon class="playlist-card__thumbnail__icon" />
+    </div>
+    <div class="playlist-card__details">
+      <h4 class="playlist-card__details__title">{{ title }}</h4>
+      <p class="playlist-card__details__meta">{{ channel }}</p>
+      <p class="playlist-card__details__description">{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import PlaylistIcon from '../../public/img/icons/svg/playlist.icon.svg';
+
+export default {
+  props: {
+    id: String,
+    title: String,
+    thumbnail: String,
+    channel: String,
+    description: String,
+  },
+  components: {
+    PlaylistIcon,
+  },
+
+  data() {
+    return {
+      subscribeCount: 0,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.playlist-card {
+  display: flex;
+  margin-bottom: 1em;
+  position: relative;
+
+  &__thumbnail {
+    position: relative;
+
+    &__icon {
+      position: absolute;
+      right: 15%;
+      top: 50%;
+      transform: translateY(-50%) translateX(50%);
+      width: 2.5em;
+      fill: rgb(235, 235, 235);
+      z-index: 2;
+    }
+
+    &::after {
+      content: '';
+      position: absolute;
+      right: 0;
+      top: 0;
+      width: 30%;
+      height: calc(100% - 0.25em);
+      background: rgba($color: #000000, $alpha: 0.5);
+    }
+  }
+
+  &__details {
+    margin-left: 1em;
+    display: flex;
+    flex-direction: column;
+
+    &__title {
+      font-weight: normal;
+    }
+
+    &__meta {
+      color: #8b8b8b;
+      margin-bottom: 0.5em;
+    }
+
+    &__description {
+      color: #8b8b8b;
+    }
+  }
+}
+</style>
