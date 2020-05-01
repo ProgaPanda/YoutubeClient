8fb9e5113f28ada3b9cc8d1ebdae7aa6c3ffff52
Khaled Albarawy <kalbarawy@raisaenergy.com>
2020-05-01 17:37:46 +0200

    [FEAT] Added options param to search api method

--- src/shared/services/api/api.service.js
+++ src/shared/services/api/api.service.js
@@ -10,11 +10,20 @@ export default {
   /**
    * Returns matching items for the search parameter from YT API
    * @param {string} searchParam
+   * @param {Object} searchOptions
    */
-  search: (searchParam) =>
-    api
-      .get(
-        `search?type=video&part=snippet&order=viewCount&q=${searchParam}+tutorial&maxResults=3&key=AIzaSyBT4Pe1v-xPdTqYBc8FJy5THAtUg90C4-0`,
-      )
-      .then((d) => console.log(d)),
+  search: (searchParam, searchOptions = {}) => {
+    const { maxResults = 10, order = 'relevance' } = searchOptions;
+    return api.get(
+      'search?part=snippet&order=viewCount&key=AIzaSyBT4Pe1v-xPdTqYBc8FJy5THAtUg90C4-0',
+      {
+        params: {
+          q: searchParam,
+          maxResults,
+          order,
+          type: 'video',
+        },
+      },
+    );
+  },
 };
