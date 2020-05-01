74cfd7cdfbb4761344c9f538fb81188b06eaf65c
Khaled Albarawy <kalbarawy@raisaenergy.com>
2020-05-01 18:17:57 +0200

    [REFACTOR] Moved keys to enviroment variables

--- src/shared/services/api/api.service.js
+++ src/shared/services/api/api.service.js
@@ -15,16 +15,13 @@ export default {
    */
   search: (searchParam, searchOptions = {}) => {
     const { maxResults = 10, order = 'relevance' } = searchOptions;
-    return api.get(
-      'search?part=snippet&order=viewCount&key=AIzaSyBT4Pe1v-xPdTqYBc8FJy5THAtUg90C4-0',
-      {
-        params: {
-          q: searchParam,
-          maxResults,
-          order,
-          type: 'video',
-        },
+    return api.get(`search?part=snippet&key=${process.env.VUE_APP_YT_KEY}`, {
+      params: {
+        q: searchParam,
+        maxResults,
+        order,
+        type: 'video',
       },
-    );
+    });
   },
 };
